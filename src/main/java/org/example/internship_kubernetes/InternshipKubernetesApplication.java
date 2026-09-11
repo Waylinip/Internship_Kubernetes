9fa0bb66b6318ec3240b384208d2a7f4e32d3ff5
danikmarukovitch <marukovitch3@icloud.com>
2026-09-11 22:13:42 +0200

    feat: initial project setup with Kubernetes manifests

---
+++ src/main/java/org/example/internship_kubernetes/InternshipKubernetesApplication.java
@@ -0,0 +1,13 @@
+package org.example.internship_kubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipKubernetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipKubernetesApplication.class, args);
+	}
+
+}
